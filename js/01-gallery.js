5b804e59fad9e89c5cdaa057312205b5befe7198
34Maryna <aupairtickets@gmail.com>
2023-03-24 13:38:57 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryEl = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+return galleryItems
+.map(({ preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+})
+.join(' ');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryEl);
+galleryContainer.addEventListener('click', onImgClick)
+
+function onImgClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG'){
+        return;
+    }
+    const modal = basicLightbox.create(
+     `<img src="${event.target.dataset.source}" width="800" height="600">`,
+     
+     {  onShow: () => window.addEventListener('keydown', onKeyPress),
+        onClose: () => window.removeEventListener ('keydown', onKeyPress),   
+    }
+     );
+ modal.show();
+  function onKeyPress (event) {
+    if(event.code === 'Escape') {
+        modal.close();
+    }
+  }  
+}
